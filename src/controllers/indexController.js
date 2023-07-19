e427bf5d7b93a7254ac42ec2db90543d7b9cfd54
cotidruetta <cotidruetta1@gmail.com>
2023-07-19 01:39:21 -0300

    /edit y el id del producto

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -9,18 +9,54 @@ const controller ={
     index : (req, res) => {
         //res.sendFile((__dirname + '/views/index.html'));
         //res.render('index');
-        libros = JSON.parse(fs.readFileSync(librosFilePath, 'utf-8')); // lee devuelta el JSON por eso es let
+        libros = JSON.parse(fs.readFileSync(librosFilePath, 'utf-8'));
 		res.render('index', {libros});
     },
-    
+
     detalledeProducto : (req, res) => {
-        //res.sendFile((__dirname + '/views/detalleProducto.html'));
-        res.render('detalleProducto');
+        let id = req.params.id;
+        libros = JSON.parse(fs.readFileSync(librosFilePath, 'utf-8'));
+        libros = libros.find(libro => libro.id == id);
+        if (libros){
+        res.render('detalleProducto', {libros});
+        }
     },
 
     cart : (req, res) => {
         //res.sendFile((__dirname + '/views/cart.html'));
         res.render('cart');
+    },
+
+    getEdit : (req, res) => {
+        let id = req.params.id;
+        libros = JSON.parse(fs.readFileSync(librosFilePath, 'utf-8'));
+        libros = libros.find(libro => libro.id == id);
+        if (libros){
+            res.render('libro-edit-form', {libros});
+        }
+        
+    },
+
+    putEdit : (req, res) => {
+        let id = req.params.id;
+        libros = JSON.parse(fs.readFileSync(librosFilePath, 'utf-8'));
+
+        l = libros.find(libro => libro.id == id);
+        
+        if (l){
+            l.titulo = req.body.titulo;
+            l.precio = req.body.precio;
+            l.descuento = req.body.descuento;
+            l.genero = req.body.genero;
+            l.descripcion = req.body.descripcion;
+        
+           
+        } 
+
+        fs.writeFileSync(librosFilePath, JSON.stringify(libros, null, ' '));
+
+        res.redirect ('/')
+        
     }
 }
 module.exports = controller;
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -6,9 +6,13 @@ const controller = require('../controllers/indexController');
 
 router.get('/', controller.index );
 
-router.get('/producto', controller.detalledeProducto );
+router.get('/producto/:id', controller.detalledeProducto );
 
 router.get('/cart', controller.cart);
+
+router.get ('/edit/:id', controller.getEdit);
+router.put ('/edit/:id', controller.putEdit);
+
 
 
 module.exports = router;
