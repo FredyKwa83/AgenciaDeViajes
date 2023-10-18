adb070c53ccd5b4289d6bdc032df789decb2d706
NanuCareaga <nanu.careaga@gmail.com>
2023-10-17 21:32:39 -0300

    ordenando y agregando las validaciones de front

---
+++ public/scripts/logicaregister.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () =>{
+   const nombreusuario = document.getElementById("n1")
+
+if (typeof nombreusuario === "string" & nombreusuario != "") {
+    console.log("todo en orden")
+}
+else{
+    console.log("algo anda mal")
+} 
+})
+
